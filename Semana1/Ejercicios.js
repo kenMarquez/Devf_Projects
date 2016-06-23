46c7bf19df3696cacc4db969d932004f556106c3
KenMarquez <darius_dam@hotmail.com>
2016-06-22 20:48:16 -0500

    Se creo carpeta semana 2 Creamos un ejemplo de un component en javascript

--- Semana1/Ejercicios.js
+++ Semana1/Ejercicios.js
@@ -1,9 +1,9 @@
 /**
  * Created by Ken on 17/06/16.
  */
-function fibonacci(n){
-    if (n == 1 || n == 0) return 1 ;
-    return fibonacci(n-1) + fibonacci(n-2);
+function fibonacci(n) {
+    if (n == 1 || n == 0) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-fibonacci()
+console.log(fibonacci(4));
--- Semana1/promesa.js
+++ Semana1/promesa.js
@@ -1,5 +1,4 @@
 /**
  * Created by Ken on 17/06/16.
  */
-const promise = require('bluebird');
 
